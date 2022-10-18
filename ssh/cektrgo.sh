9a514884711baf916b246358db6e0d0f28bf627d
copy47 <112587135+copy47@users.noreply.github.com>
2022-10-18 08:12:25 +0700

    Add files via upload

---
+++ ssh/cektrgo.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+#GREEN='\033[0;32m'
+#ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+#CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+off='\x1b[m'
+# ==========================================
+# Getting
+
+clear
+echo -n > /tmp/iptrojango.txt
+data=( `cat /var/log/trojan.log | grep -w 'authenticated as' | awk '{print $7}' | sort | uniq`);
+echo -e "${red}============================${NC}";
+echo "      TROJAN USER LOGIN"
+echo -e "${red}============================${NC}";
+for akun in "${data[@]}"
+do
+data2=( `lsof -n | grep -i ESTABLISHED | grep trojan | awk '{print $9}' | cut -d':' -f2 | grep -w 445 | cut -d- -f2 | grep -v '>127.0.0.1' | sort | uniq | cut -d'>' -f2`);
+echo -n > /tmp/iptrojan.txt
+for ip in "${data2[@]}"
+do
+jum=$(cat /var/log/trojan.log | grep -w $akun | awk '{print $4}' | cut -d: -f1 | grep -w $ip | sort | uniq)
+if [[ -z "$jum" ]]; then
+echo > /dev/null
+else
+echo "$jum" > /tmp/iptrojan.txt
+fi
+done
+jum2=$(cat /tmp/iptrojan.txt | nl)
+echo "User : $akun";
+echo "$jum2";
+echo -e "============================${NC}";
+echo "Script By Arya Blitar"
+rm -rf /tmp/other.txt
+
