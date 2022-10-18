4da7e705a04d3f9c982adf15c377722c3eb87ee4
copy47 <112587135+copy47@users.noreply.github.com>
2022-10-18 08:49:22 +0700

    Add files via upload

--- xray/cek-ws.sh
+++ xray/cek-ws.sh
@@ -1,28 +1,33 @@
 #!/bin/bash
-#######
-red='\e[1;31m'
-green='\e[0;32m'
-NC='\e[0m'
+# My Telegram : 
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
+
 clear
-#echo -n /var/log/xray/access.log
 echo -n > /tmp/other.txt
-data=( `cat /etc/xray/config.json | grep '^###' | cut -d ' ' -f 2 | sort | uniq`);
-
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "${green}         Vmess User Login         \E[0m"
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-
+data=( `cat /etc/xray/config.json | grep '^####' | cut -d ' ' -f 2`);
+echo "----------------------------------------";
+echo "---------=[ Vmess User Login ]=---------";
+echo "----------------------------------------";
 for akun in "${data[@]}"
 do
 if [[ -z "$akun" ]]; then
 akun="tidakada"
 fi
-
 echo -n > /tmp/ipvmess.txt
 data2=( `netstat -anp | grep ESTABLISHED | grep tcp6 | grep xray | awk '{print $5}' | cut -d: -f1 | sort | uniq`);
 for ip in "${data2[@]}"
 do
-
 jum=$(cat /var/log/xray/access.log | grep -w $akun | awk '{print $3}' | cut -d: -f1 | grep -w $ip | sort | uniq)
 if [[ "$jum" = "$ip" ]]; then
 echo "$jum" >> /tmp/ipvmess.txt
@@ -32,7 +37,6 @@ fi
 jum2=$(cat /tmp/ipvmess.txt)
 sed -i "/$jum2/d" /tmp/other.txt > /dev/null 2>&1
 done
-
 jum=$(cat /tmp/ipvmess.txt)
 if [[ -z "$jum" ]]; then
 echo > /dev/null
@@ -40,17 +44,13 @@ else
 jum2=$(cat /tmp/ipvmess.txt | nl)
 echo "user : $akun";
 echo "$jum2";
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo "----------------------------------------"
 fi
 rm -rf /tmp/ipvmess.txt
 done
-
 oth=$(cat /tmp/other.txt | sort | uniq | nl)
 echo "other";
 echo "$oth";
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo "----------------------------------------"
+echo "Script By Etil"
 rm -rf /tmp/other.txt
-echo ""
-read -n 1 -s -r -p "Press any key to back on menu"
-
-menu
