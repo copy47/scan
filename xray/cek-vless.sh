ce7ef5f44c40cad99bb56a3bc8f736422ee54053
copy47 <112587135+copy47@users.noreply.github.com>
2022-10-18 13:54:11 +0700

    Add files via upload

--- xray/cek-vless.sh
+++ xray/cek-vless.sh
@@ -1,34 +1,32 @@
 #!/bin/bash
-# My Telegram : 
-# ==========================================
-# Color
-RED='\033[0;31m'
-NC='\033[0m'
-GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-LIGHT='\033[0;37m'
-# ==========================================
-# Getting
-
-clear
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+MYIP=$(wget -qO- ipinfo.io/ip);
+echo "Checking VPS"
+IZIN=$( curl https://Etil-arya.github.io/izin | grep $MYIP )
+if [ $MYIP = $IZIN ]; then
+echo -e "${green}Permission Accepted...${NC}"
+else
+echo -e "${red}Permission Denied!${NC}";
+echo "Only For Premium Users"
+exit 0
+fi
 echo -n > /tmp/other.txt
-data=( `cat /etc/xray/config.json | grep '^#' | cut -d ' ' -f 2`);
-echo "----------------------------------------";
-echo "---------=[ Vless User Login ]=---------";
-echo "----------------------------------------";
+data=( `cat /etc/v2ray/vless.json | grep '^###' | cut -d ' ' -f 2`);
+echo "-------------------------------";
+echo "-----=[ Vless User Login ]=-----";
+echo "-------------------------------";
 for akun in "${data[@]}"
 do
 if [[ -z "$akun" ]]; then
 akun="tidakada"
 fi
 echo -n > /tmp/ipvless.txt
-data2=( `netstat -anp | grep ESTABLISHED | grep tcp6 | grep xray | awk '{print $5}' | cut -d: -f1 | sort | uniq`);
+data2=( `netstat -anp | grep ESTABLISHED | grep tcp6 | grep v2ray | awk '{print $5}' | cut -d: -f1 | sort | uniq`);
 for ip in "${data2[@]}"
 do
-jum=$(cat /var/log/xray/access.log | grep -w $akun | awk '{print $3}' | cut -d: -f1 | grep -w $ip | sort | uniq)
+jum=$(cat /var/log/v2ray/access2.log | grep -w $akun | awk '{print $3}' | cut -d: -f1 | grep -w $ip | sort | uniq)
 if [[ "$jum" = "$ip" ]]; then
 echo "$jum" >> /tmp/ipvless.txt
 else
@@ -44,13 +42,13 @@ else
 jum2=$(cat /tmp/ipvless.txt | nl)
 echo "user : $akun";
 echo "$jum2";
-echo "----------------------------------------"
+echo "-------------------------------"
 fi
-rm -rf /tmp/ipvless.txt
+rm -rf /tmp/ipvmess.txt
 done
 oth=$(cat /tmp/other.txt | sort | uniq | nl)
 echo "other";
 echo "$oth";
-echo "----------------------------------------"
-echo "Script By Etil"
+echo "-------------------------------"
 rm -rf /tmp/other.txt
+
